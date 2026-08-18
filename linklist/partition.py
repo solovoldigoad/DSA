d125045aef25ac12e222b7f4594ca16e8b862a0c
kush chauhan <kushchauhan2004@gmail.com>
2026-08-18 23:02:26 +0530

    link list

---
+++ linklist/partition.py
@@ -0,0 +1,19 @@
+small = ListNode(0)
+large = ListNode(0)
+
+s = small
+l = large
+
+while head:
+    if head.val < x:
+        s.next = head
+        s = s.next
+    else:
+        l.next = head
+        l = l.next
+
+    head = head.next
+
+l.next = None
+s.next = large.next
+
